4018105768c64a3fc9e95a943bc2cc233a46ef7d
goldi devlopment <goldikumari54htce@gmail.com>
2025-04-30 08:40:18 +0530

    overloding in order polymorphism

---
+++ overloding_polymorphism.py
@@ -0,0 +1,19 @@
+class Order:
+    def place_order_1(self, item1):
+        print("Order placed for:- ")
+        print(f"- {item1}")
+
+    def place_order_2(self, item1, item2):
+        print("Order placed for:- ")
+        print(f"- {item1}")
+        print(f"- {item2}")
+
+    def place_order_3(self, item1, item2, item3):
+        print("Order placed for:- ")
+        print(f"- {item1}")
+        print(f"- {item2}")
+        print(f"- {item3}")
+order = Order()
+order.place_order_1("burger")                         
+order.place_order_2("pizza", "pasta")                  
+order.place_order_3("pizza", "pasta", "juice")        
